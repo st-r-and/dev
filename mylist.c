d2929fed203106b9605710c5a59d91133bf4730c
Steve Reichel <steve@fam-reichel.de>
2012-12-27 15:27:11 +0100

    mylist ausgabe_pers + ausgabe_eig add

--- mylist.c
+++ mylist.c
@@ -13,7 +13,7 @@ struct eigenschaft {
 struct person {
   char nachname[MAX];
   char vorname[MAX];
-  struct eigenschaft eig;
+  struct eigenschaft *eig;
   struct person *next;
 };
 
@@ -26,23 +26,72 @@ void pers_anhaengen(char *n, char *v){
       fprintf(stderr, "Kein Speicherplatz");
       return;
     }
+    strcpy(pers->nachname, n);
+    strcpy(pers->vorname, v);
+    pers->eig = NULL;
+    pers->next = NULL;
+  } else {
+    pointer = pers;
+    while(pointer->next != NULL)
+      pointer = pointer->next;
+    if((pointer->next = malloc(sizeof(struct person))) == NULL){
+      fprintf(stderr, "Kein Speicherplatz");
+      return;
+    }
+    pointer = pointer->next;
+    strcpy(pointer->nachname, n);
+    strcpy(pointer->vorname, v);
+    pointer->eig = NULL;
+    pointer->next = NULL;
   }
 }
 
-void ausgabe(void){
+void ausgabe_eig(char *n, char *v){
   printf("Hallo\n");
+}
+
+void ausgabe(void){
+  struct person *pointer = pers;
+  while(pointer != NULL) {
+    printf("%s - %s ", pointer->nachname, pointer->vorname);
+    if(pointer->eig != NULL){
+      ausgabe_eig(pointer->nachname, pointer->vorname);
+    } else {
+      printf("keine Eigenschaften");
+    }
+    printf("\n");
+    pointer = pointer->next;
+  }
+}
+
+void pers_eingabe(void){
+  char naname[MAX], voname[MAX];
+  char *pointer;
+  printf("Nachname   : ");
+  fgets(naname, MAX, stdin);
+  pointer = strrchr(naname, '\n');
+  *pointer = '\0';
+  printf("Vorname    : ");
+  fgets(voname, MAX, stdin);
+  pointer = strrchr(voname, '\n');
+  *pointer = '\0';
+  //getchar();
+  pers_anhaengen(naname, voname);
 }
 
 void menu(void){
   int wahl;
   do {
     printf("\n1 : Print\n");
+    printf("2 : Write person\n");
     printf("Ende: 9\n");
     printf("Auswahl: ");
     scanf("%d", &wahl);
     getchar();
     switch(wahl){
     case 1 : ausgabe();
+      break;
+    case 2 : pers_eingabe();
       break;
     case 9 :
       break;
